74bfa97dc2f3a9c03e2677a89e1f137b9b05ce6e
Elyorbek <81812657+Abdulametov@users.noreply.github.com>
2021-04-02 18:45:27 +0500

    yangi fayllar qo'shildi
    c:disk ichida dars_12 papkasini ichiga  fayl yuklandi

---
+++ dars_12/dars#12.py
@@ -0,0 +1,31 @@
+"""
+02/04/2021
+Phyton lessons
+#12-dars: Xatolar
+Muallif: Elyor Abdulametov
+Web sahifa: https://python.sariq.dev
+"""
+
+mahsulotlar = ['un', "yog'", "sovun", 'tuxum', 'piyoz',
+               'kartoshka', 'olma', 'banan', 'uzum', 'qovun']
+
+
+savat = []
+for n in range(5):
+    savat.append(input(f'Savatga {n+1}-mahsulotni qo\'shing: '))
+
+bor_mahsulotlar = []
+mavjud_emas = []
+for mahsulot in savat:
+    if mahsulot in mahsulotlar:
+        bor_mahsulotlar.append(mahsulot)
+    else:
+        mavjud_emas.append(mahsulot)
+
+if mavjud_emas:
+  print("Do'konimizda quyidagi mahsulotlar yo'q:")
+for mahsulot in mavjud_emas:
+  print(mahsulot)
+else:
+  print("Siz so'ragan barcha mahsulotlar do'konimizda bor")
+    
